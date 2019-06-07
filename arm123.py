d092fa0f56c2922a7fa1e69bba332c8160267cc6
balakumardevil <42169755+balakumardevil@users.noreply.github.com>
2019-06-07 12:32:35 +0530

    Update arm123.py

--- arm123.py
+++ arm123.py
@@ -5,9 +5,8 @@ for n in range(n,m):
     c=0 
     while(n!=0):
         rem=n%10
-        a=rem**3
+        c+=rem**3
         n=n//10
-        c=a+c
     if(c==t):
         print(c,end=' ')
         
